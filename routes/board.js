b6ca9530e6b227003d21161d06e33dcf9d730067
ImMin5 <rlaalsgh5151@naver.com>
2021-10-08 00:52:14 +0900

    add function delete,update,paging

--- routes/board.js
+++ routes/board.js
@@ -21,7 +21,7 @@ router.get('/list',function(req,res,next) {
 //글쓰기 페이지
 router.get('/write',function(req,res,next) {
 	res.render('write',{title : "게시판 글쓰기"});
-})
+});
 
 //글쓰기 버튼 클릭시
 router.post('/write',function(req,res,next){
@@ -45,6 +45,61 @@ router.get('/read/:idx',function(req,res,next){
 	conn.query(sql,[idx],function(err,row){
 		if(err)console.err(err);
 		res.render('read',{title : "글 상세",row:row[0]});
+	});
+});
+
+//글 수정
+router.post('/update',function(req,res,next){
+	var idx = req.body.idx;
+	var name = req.body.name;
+	var title = req.body.title;
+	var content = req.body.content;
+	var datas = [name,title,content,idx];
+
+	var sql = "update board set name =? ,title=?, content=?, modidate=now() where idx=?";
+	conn.query(sql, datas, function(err,result){
+		if(err)console.err(err);
+		if(result.affectedRows == 0){
+			res.send("<script>alert('패스워드 불일치'); history.back();</script>");
+		}
+		else
+		{
+			res.redirect('/board/read/'+idx);
+		}
+
+	});
+});
+
+//글삭제
+router.post('/delete',function(req,res,next){
+	var idx = req.body.idx;
+	var datas = [idx];
+
+	var sql = "delete from board where idx =?";
+	conn.query(sql,datas,function(err,result){
+		if(err)console.err(err);
+		if(result.affectedRows == 0){
+			res.send("<script>alert('패스워드 불일치');history.back();</script>");
+		}
+		else 
+		{
+			res.redirect('/board/list');
+		}
+	});
+
+
+});
+
+
+//페이징
+router.get('/page/:page',function(req,res,next){
+	var page = req.params.page;
+	var sql = "select idx,name,title,content, hit,date_format(modidate,'%Y-%m-%d %H:%i:%s') modidate,"+
+	" date_format(regdate,'%Y-%m-%d %H:%i:%s')regdate from board";
+	conn.query(sql,function(err,rows){
+		if(err)console.err(err);
+		res.render('page',{title : "게시판 리스트",rows:rows, page:page, length:rows.length-1, page_num:1,pass:true});
+		console.log(rows.length-1);
 	});
 });
 
