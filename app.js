9c4aae75603767c49a186f878c16ba62c4ea3aa4
ImMin5 <rlaalsgh5151@naver.com>
2021-10-12 21:43:30 +0900

    naver login

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var passport = require('passport');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,7 +15,6 @@ var boardRouter = require('./routes/board');
 var loginRouter = require('./routes/login');
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -23,12 +24,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  key: 'sid',
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/form',formRouter);
 app.use('/mysql',mysqlRouter);
 app.use('/board',boardRouter)
+
+//로그인
 app.use('/login', loginRouter);
 
 //static file
--- routes/login.js
+++ routes/login.js
@@ -2,10 +2,55 @@ var express = require('express');
 var router = express.Router();
 var mysql_odbc = require('../db/db_connect');
 var conn = mysql_odbc.init();
-
+var passport = require('passport'); //passport 추가 
+var NaverStrategy = require('passport-naver').Strategy;
+var config_naver = require('../config/secret').naver;
 
 router.get('/',function(req,res,next) {
 	res.render('login',{title:"로그인"});
+});
+
+
+router.get('/naver',passport.authenticate('naver',null),function(req,res){
+	console.log("/login/naver");
+});
+
+//처리 후 callback 처리 부분 성공/실패 시 리다이렉트 설정 
+router.get('/naver/callback', passport.authenticate('naver', { 
+	successRedirect: '/board/list', 
+	failureRedirect: '/login'
+	 }) 
+);
+
+passport.use(new NaverStrategy({
+    clientID: config_naver.client_id,
+    clientSecret: config_naver.client_secret,
+    callbackURL: config_naver.callback_url
+}, 
+function(accessToken, refreshToken, profile, done) {
+    process.nextTick(function () {
+
+        var user = {
+            name: profile.displayName,
+            email: profile.emails[0].value,
+            provider: 'naver',
+            naver: profile._json
+        };
+        console.log("user=");
+        console.log(user);
+    	
+        return done(null, profile);
+    	});
+	}
+));
+
+//failed to serialize user into session 에러 발생 시 아래의 내용을 추가 한다.
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
 });
 
 module.exports = router;
